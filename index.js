4220f3c9b0d0f7c8a73f496c9cc6df02df446f61
Alfonso Ruzafa <alfonso.ruzafa@movo.me>
2020-02-21 12:47:57 +0100

    add asking behavior

--- index.js
+++ index.js
@@ -85,18 +85,47 @@ module.exports.templateTags = [{
       displayName: 'Color - HTML',
       value: 'color/html'
     }]
+  }, {
+    displayName: 'When to ask for a value',
+    help: 'Controls when the asking dialog must be shown to the user',
+    type: 'enum',
+    options: [{
+      displayName: 'Ask always',
+      value: 'ask/blank'
+    }, {
+      displayName: 'Ask always, but suggest the last entered value',
+      value: 'ask/stored'
+    }, {
+      displayName: 'Ask always, but suggest the default value',
+      value: 'ask/default'
+    }, {
+      displayName: 'Ask once, and use the value for further requests',
+      value: 'once/stored'
+    }]
+  }, {
+    displayName: 'Options - Reserved for future use',
+    help: 'Options for the param',
+    hide: () => true,
+    type: 'string'
   }],
 
-  async run (context, name, typeFormat) {
+  async run (context, name, typeFormat, askBehavior, paramOpts) {
     const [type] = getTypeFormat(typeFormat);
     const paramHash = crypto.createHash('md5').update(name).digest('hex');
     const storageKey = `${context.meta.requestId}.${paramHash}.${type}`;
     const storedValue = await context.store.getItem(storageKey);
+    let defaultValue = '';
+
+    if (askBehavior === 'once/stored' && storedValue !== '') {
+      return storedValue;
+    } else if (askBehavior === 'ask/stored') {
+      defaultValue = storedValue || '';
+    }
     const inputType = getHtmlInputType(typeFormat);
     const [title, description] = extractParamComponents(name)
     const value = await context.app.prompt(title, {
       label: description,
-      defaultValue: storedValue || '',
+      defaultValue,
       inputType,
       selectText: true
     });
